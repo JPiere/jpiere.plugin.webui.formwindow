fc8af2b9451bf20c645aea2e6bef754ce3be5ef1
jpiere <h.hagiwara@compiere-distribution-lab.net>
2021-10-23 10:00:19 +0900

    IDEMPIERE-4964:Status line message does not update when changing the detail on Sales Order

--- src/jpiere/plugin/webui/adwindow/JPiereCompositeADTabbox.java
+++ src/jpiere/plugin/webui/adwindow/JPiereCompositeADTabbox.java
@@ -187,7 +187,7 @@ public class JPiereCompositeADTabbox extends JPiereAbstractADTabbox
 						showLastError();
 					} else {
 						tabPanel.getGridTab().dataRefreshAll(true, true);
-						tabPanel.getGridTab().refreshParentTabs();
+						tabPanel.getGridTab().refreshParentTabs(true);
 					}
 				}
 				else if (DetailPane.ON_DELETE_EVENT.equals(event.getName())) {
