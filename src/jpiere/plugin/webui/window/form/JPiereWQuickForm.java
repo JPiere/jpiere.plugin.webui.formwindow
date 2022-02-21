5d701ab12f24c0ba14b3a972cc75718f1a15d262
jpiere <h.hagiwara@compiere-distribution-lab.net>
2022-02-21 15:28:18 +0900

    Maintain

--- src/jpiere/plugin/webui/window/form/JPiereWQuickForm.java
+++ src/jpiere/plugin/webui/window/form/JPiereWQuickForm.java
@@ -9,6 +9,9 @@
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *                                                                            * 
+ * Contributor:                                                               * 
+ *   Andreas Sumerauer                                                        * 
  *****************************************************************************/
 
 package jpiere.plugin.webui.window.form;
@@ -62,8 +65,6 @@ public class JPiereWQuickForm extends Window implements EventListener <Event>, D
 	 *
 	 */
 	private static final long serialVersionUID = -5363771364595732977L;
-
-	public Trx						trx					= null;
 
 	private Borderlayout			mainLayout			= new Borderlayout();
 	private JPiereAbstractADWindowContent	adWinContent		= null;
