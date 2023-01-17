332c570256ab194f51fcdbc4fbd3e3df145d77b9
jpiere <h.hagiwara@compiere-distribution-lab.net>
2023-01-17 20:22:54 +0900

    Maintain to IDEMPIERE-5535 Warning dialog shown wrongly

--- src/jpiere/plugin/webui/adwindow/JPiereAbstractADWindowContent.java
+++ src/jpiere/plugin/webui/adwindow/JPiereAbstractADWindowContent.java
@@ -1907,7 +1907,7 @@ public abstract class JPiereAbstractADWindowContent extends AbstractUIPart imple
         		isImporting = gridTable.isImporting();
         	}
         	if (!isImporting) {
-        		Dialog.warn(curWindowNo, e.getAD_Message(), e.getInfo());
+        		Dialog.warn(curWindowNo, e.getAD_Message(), e.getInfo(), null);
         		e.setConfirmed(true);   //  show just once - if MTable.setCurrentRow is involved the status event is re-issued
         	}
         }
